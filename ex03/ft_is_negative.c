2d64b1bb7a578263bf81e7304b31dd22db986a3f
bmacheke <67003918+bmacheke@users.noreply.github.com>
2020-06-23 14:32:54 +0200

    Add files via upload

---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write (1 , &c, 1);
+}
+
+void ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('N');
+	}
+	else
+	{
+		ft_putchar('P');
+	}
+	ft_putchar('\n');
+}
+
+int main(void)
+{
+	int n;
+	ft_is_negative(n);
+	return 0;
+}
